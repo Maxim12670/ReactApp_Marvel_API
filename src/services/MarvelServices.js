cebd2139224c78af92ada454bc663b86adb58b0e
Максим <akaikinmax@mail.ru>
2023-11-03 14:12:06 +0700

    docs(taks.txt): задание 3

--- src/services/MarvelServices.js
+++ src/services/MarvelServices.js
@@ -29,7 +29,8 @@ class MarvelServices {
             description: char.description ? `${char.description.slice(0, 210)}...` : 'Описания данного персонажа нет!',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
-            wiki: char.urls[1].url
+            wiki: char.urls[1].url,
+            comics: char.comics.items  
         }
     }
 }
